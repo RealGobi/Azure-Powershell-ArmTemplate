677b78f701644d050d76190d1c9253c243e625c2
Jimmy Svensson <43778332+RealGobi@users.noreply.github.com>
2021-05-26 23:56:04 +0200

    Update closeVM.ps1
    comments

--- closeVM.ps1
+++ closeVM.ps1
@@ -16,6 +16,8 @@
     
   $VM = Get-AzResource -TagName "School-demo" -TagValue "13.4.67" && Stop-AzVM -Name $VM.Name -ResourceGroupName $VM.ResourceGroupName -confirm:$false
 
+
+// working version
   $VMs = Get-AzResource -TagName "School-demo" -TagValue "13.4.67"; foreach($VM in $VMs) { Stop-AzVM -Name $VM.Name -ResourceGroupName $VM.ResourceGroupName -Confirm:$false -force }
 
 
