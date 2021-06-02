a314a7efb406281d3a1e937efa7d00d9429e6806
Jimmy Svensson <jimmy888swe@gmail.com>
2021-06-02 20:23:20 +0200

    problems

--- deplos-vm.ps1
+++ deplos-vm.ps1
@@ -1,6 +1,13 @@
+
+<# 
+Test-AzResourceGroupDeployment `
+-ResourceGroupName $rg `
+-TemplateFile 'vm.json'`
+-TemplateParameterFile './vm.parameter.json' #>
 $rg = 'jimmy-AA-Resource'
 
-New-AzResourceGroupDeployment `
-    -Name 'new-vm' `
+    New-AzResourceGroupDeployment `
+    -Name "new-vm-${get-date -Format yyMMddHHmm}" `
     -ResourceGroupName $rg `
-    -TemplateFile 'vm.json' 
+    -TemplateFile 'vm.json' `
+    -TemplateParameterFile './vm.parameter.json' 
